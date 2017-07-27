8a77f47fdb4bf768f5fbdeb789757c2276d51bca
NaediVieira <naedivieira@gmail.com>
2017-07-27 18:12:04 -0400

    teste de rotina

--- src/test/java/br/com/qualister/triangulo/TrianguloBasicoUnitTest.java
+++ src/test/java/br/com/qualister/triangulo/TrianguloBasicoUnitTest.java
@@ -9,16 +9,19 @@ public class TrianguloBasicoUnitTest {
 	@Test
 	public void testeTrianguloEquilatero() throws TrianguloNaoPreenchidoException {
 		assertEquals("O triângulo é Equilátero", Triangulo.calcular(3, 3, 3));
+		System.out.println("teste");
 	}
 	
 	@Test
 	public void testeTrianguloEscaleno() throws TrianguloNaoPreenchidoException {
 		assertEquals("O triângulo é Escaleno", Triangulo.calcular(3, 6, 9));
+		System.out.println("teste1");
 	}
 	
 	@Test
 	public void testeTrianguloIsosceles() throws TrianguloNaoPreenchidoException {
 		assertEquals("O triângulo é Isósceles", Triangulo.calcular(3, 6, 6));
+		System.out.println("teste2");
 	}
 
 }
